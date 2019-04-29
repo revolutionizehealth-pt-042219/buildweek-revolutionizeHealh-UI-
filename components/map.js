f7a30a22a5b19f1e44dd1563b668dc4b810c2718
Ernesto Pena <ernestopenad@gmail.com>
2019-04-29 07:31:03 -0500

    90% desktop is completed

--- components/map.js
+++ components/map.js
@@ -1,13 +1,27 @@
+if ("geolocation" in navigator) {
+    
+    navigator.geolocation.getCurrentPosition(function(position) {
+        show_map(position.coords.latitude, position.coords.longitude);
+      });
+}else {
+    let browser_error = document.querySelector('.browser_error');
+    browser_error.classList.toggle('show_message');
+}
 
-var mymap = L.map('mapid').setView([29.961815 , -95.5304], 13);
+function show_map(lat , long) {
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.streets'
-}).addTo(mymap);
+    var mymap = L.map('mapid').setView([lat , long], 13);
+    
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.streets'
+    }).addTo(mymap);
+    
+     L.marker([lat , long]).addTo(mymap).bindPopup("<b>Dear Lambda Student.<br><br/>This is approximately where you are located right now. </br>Unfortunately, neither Mapquest or LeafLet are able to provide your exact location. Use the search to pull your<br> location from the US Census Bureau API.").openPopup();
+     var popup = L.popup();
+    
+}
 
- L.marker([29.961815 , -95.5304]).addTo(mymap).bindPopup("<b>Hello, This is QCTV</b><br>We are located at</br><br> 7038 Belgold St</br>Houston TX 77066").openPopup();
- var popup = L.popup();
