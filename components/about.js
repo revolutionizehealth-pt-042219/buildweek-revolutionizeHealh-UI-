157150050045a5271666ae6eafbd98af23553992
Ernesto Pena <ernestopenad@gmail.com>
2019-05-02 14:16:27 -0500

    About page improvements

---
+++ components/about.js
@@ -0,0 +1,8 @@
+const techs = document.querySelectorAll('.tech_logos');
+
+techs.forEach(function(e){
+    e.addEventListener('mouseover', function(event){
+        console.log('test');
+    })
+})
+
