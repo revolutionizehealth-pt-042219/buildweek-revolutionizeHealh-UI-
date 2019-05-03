3438c7b44c1c645d3bfdd1377a14d975b1eb7ce1
Ernesto Pena <ernestopenad@gmail.com>
2019-05-02 20:05:27 -0500

    Working on the Google Map

--- components/map_google.js
+++ components/map_google.js
@@ -1,3 +1,4 @@
+//WHEN USER VISIT "SEARCH" PAGE FOR FIRST TIME
 if ("geolocation" in navigator) {
     
     navigator.geolocation.getCurrentPosition(function(position) {
@@ -8,13 +9,27 @@ if ("geolocation" in navigator) {
 }else {
     let browser_error = document.querySelector('.browser_error');
     browser_error.classList.toggle('show_message');
+
 }
 
+
 function show_map(lat , long) {
-    var map;
-     map = new google.maps.Map(document.getElementById('mapid'), {
-        center: {lat: lat, lng: long},
-        zoom: 13
-      });    
+    var my_home;
+    var my_lat_long = {lat: lat, lng: long};
+    console.log(my_lat_long)
+    my_home = new google.maps.Map(document.getElementById('mapid'), {
+        center: my_lat_long,
+        zoom: 13,
+      });
+      
+      var marker = new google.maps.Marker({
+          position: my_lat_long,
+          map: my_home,
+          title: 'Hello World!',
+          //tittle: 'Dear Lambda Student, I could not finish the search functionality on the side but You can hopefully see where you are right know'
+      });
 }
+
+
+
 
